5805a0d0db590b576872712f23a1ea055a347b68
ecleaver <37559968+ecleaver@users.noreply.github.com>
2021-06-06 12:49:53 -0400

    Uploaded numbers
    How to use numbers in javascript
    
    Author:  Erin

--- guessgame.js
+++ guessgame.js
@@ -4,3 +4,22 @@
 let num1 = 5.7;
 
 console.log(5 * 10)
+console.log(50/5)
+
+num1 = 10;
+num1 = num1+1;
+console.log(num1);
+//intcrement num1 by 1
+num1++;
+console.log(num1);
+//decrement num1 by 1
+num1--;
+console.log(num1);
+
+// Divide, multiply *, reminder %
+console.log(num1 % 5)
+
+// intcrement/decrement by anynumber.
+num1 +=10;
+
+console.log(num1)
