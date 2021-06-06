528f34dde9e6f9d4480bf9dfabf10a112ecb0775
ecleaver <37559968+ecleaver@users.noreply.github.com>
2021-06-06 13:09:09 -0400

    update

--- guessgame.js
+++ guessgame.js
@@ -1,2 +1,28 @@
-// functions in javascript 
+/* functions in javascript 
+1.  Create a function 
+2.  Call the function
+*/
 
+function fun() {
+    console.log('This is an function');
+}
+
+
+// takes in a name and returns hello, followed by your name
+let name = prompt('What i your name?')
+const greeting = (name) => {
+    let result = `Hello ${name}`
+    console.log(result)
+}
+
+
+fun();
+
+// greeting()
+const sumNumbers = (num1, num2) => {
+    return num1 + num2;
+}
+
+console.log(sumNumbers(4, 5))
+
+greeting(name);
