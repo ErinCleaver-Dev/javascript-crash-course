91974c6761ac838b8f3fa722907b5502d0bf0222
ecleaver <37559968+ecleaver@users.noreply.github.com>
2021-06-07 07:55:14 -0400

    data types

---
+++ DataTypes.js
@@ -0,0 +1,16 @@
+// Data types in javascript
+
+// number data type
+let yourAge = 18;
+//string data type
+let yourName = 'Bob';
+// object data type
+let name = {first: 'John', last: 'Doe'}; 
+// array data type
+let groceries = ['apple', 'banana', 'oranges'];
+// undefined data type
+let random;
+// null data type
+let nothing = null;
+// boolean data type
+let boolean = false;
